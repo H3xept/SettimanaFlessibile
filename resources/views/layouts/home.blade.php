218293c8cb2452860bfcc2989af1a2d9ebb2d77e
vagrant <vagrant@homestead>
2015-12-12 16:41:45 +0000

    Course migration. + Partial for courses + UI Improvements

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,4 +1,35 @@
 @extends('layouts.master')
 @section('content')
-<h1>hey there </h1>
+
+<div id="list-id" class="jumbotron">
+
+	<div class="form-group form-group-lg">
+	  <input class="form-control fuzzy-search uni-search" type="text" id="search" placeholder="Ricerca corso per nome o referenti.">
+	</div><hr>
+	@for ($i = 0; $i < 10; $i++)
+	<ul class="list" style="list-style-type:none; padding:0px;">
+		@include('partials._course')
+	</ul>
+	@endfor
+</div>
+
+<script>
+
+var fuzzyOptions = {
+  searchClass: "fuzzy-search",
+  location: 0,
+  distance: 100,
+  threshold: 0.4,
+    multiSearch: true
+};
+var options = {
+  valueNames: ['name','b_code','authors','price'],
+  plugins: [
+    ListFuzzySearch()
+  ]
+};
+
+var listObj = new List('list-id', options);
+
+</script>
 @stop
---
+++ resources/views/partials/_course.blade.php
@@ -0,0 +1,11 @@
+<div class="panel panel-default">
+  <div class="panel-heading table-layout:fixed;"> 
+  <p class="lead" style="font-size:20px; margin-bottom:0px;">[Nuntius] Introduzione alla lingua giapponese.<span class="label label-default pull-right"> Progressivo </span></p> 
+  </div>
+
+  <div class="panel-body">
+    <button class="btn btn-info">Info</button>
+    <button class="btn btn-default">Iscriviti</button>
+
+  </div>
+</div>
