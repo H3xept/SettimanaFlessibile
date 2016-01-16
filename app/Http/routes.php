1bf590361532c24254f4df313e103af2c00e7fd6
H3xept <h3xept@gmail.com>
2016-01-16 18:44:46 +0100

    [+]FIXES

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -51,6 +51,7 @@ Route::get('/administration',['as'=>'admin',function(){
 Route::post('/administration/dbimport',['as'=>'admin.installDB',function(){
 	if(userIsAdmin() == NULL) return redirect(route("home"))->withErrors(["Non hai i privilegi necessari per l'amministrazione."]);
 	DB::table('courses')->truncate();
+	DB::table('stripe_user')->truncate();
 	foreach(CourseInstaller::all() as $course_installer)
 	{
 		if(Course::where('name','=',$course_installer->name)->first() != NULL)
