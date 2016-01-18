b49278a8e3e1dceb033061e48985cc2e9062db23
H3xept <h3xept@gmail.com>
2016-01-18 20:48:12 +0100

    [*]?

--- app/User.php
+++ app/User.php
@@ -42,6 +42,7 @@ class User extends Model implements AuthenticatableContract,
 
     public function referringInStripe($stripe_number)
     {
+        dd($this->courses);
         foreach ($this->courses as $course) {
 
         if($this->username == "matteosimonetti4l"){
