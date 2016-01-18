2482bf9bc5d627d73eb7419d198138a0cb90ae5a
professore <professore@retegalilei.local>
2016-01-18 12:57:41 +0100

    [*]

--- resources/views/layouts/admin/genCallCourse.blade.php
+++ resources/views/layouts/admin/genCallCourse.blade.php
@@ -12,7 +12,7 @@
 		    <?php $stripes = $course->stripes; ?>
 			@for($i = 0; $i < 9; $i++)
 
-			@if(isset($stripes[$i+1]))
+			@if(array_key_exists($stripes[$i+1]))
 				<?php dd("AYY"); ?>
 				<table class="table">
 			    <thead>
