1101a242b719c687e04963facf87049327ef2ab5
professore <professore@retegalilei.local>
2016-01-18 11:59:39 +0100

    [*]Final

--- resources/views/layouts/admin/genCall.blade.php
+++ resources/views/layouts/admin/genCall.blade.php
@@ -4,7 +4,7 @@
 	    <link href="{{ asset('css/bootstrap-theme.min.css')}}" rel="stylesheet">
 	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	</head>
-	<body style="font-size:14px;">
+	<body style="font-size:12px;">
 	<div class="container-fluid">
 		<div class="row-fluid">
 		<div class="col-md-12">
