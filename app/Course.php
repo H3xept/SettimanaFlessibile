9c86caeb8103426e905c826a5ab7176af61d04b4
professore <professore@retegalilei.local>
2016-01-19 10:32:06 +0100

    [+] Course Full Fix

--- app/Course.php
+++ app/Course.php
@@ -81,8 +81,6 @@ class Course extends Model
 
     public function studentsSignedInStripeCall($stripe_call)
     {
-        if($this->single_stripe) $stripe_call=1;
-
         $stripe = $this->stripes()->where('stripe_call', '=',$stripe_call)->first();
 
         if($stripe == null)
