3ba250266cce2595c17562d4cb5a0e12dcbec840
H3xept <h3xept@gmail.com>
2016-01-19 23:30:30 +0100

    Ayy

--- resources/views/layouts/courses.blade.php
+++ resources/views/layouts/courses.blade.php
@@ -17,6 +17,9 @@
 
 <ul class="list" style="list-style-type:none; padding:0px;">
 	@foreach($courses as $course)
+  @if($course->isFull()) 
+  @continue 
+  @endif
     <li>
       @include('partials._course')
     </li>
