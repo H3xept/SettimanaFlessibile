2758ce8831b94849f8a20a0905f4ef3c42f32bd5
H3xept <h3xept@gmail.com>
2016-01-19 17:51:01 +0100

    TEMP

--- resources/views/layouts/courses.blade.php
+++ resources/views/layouts/courses.blade.php
@@ -17,7 +17,7 @@
 
 <ul class="list" style="list-style-type:none; padding:0px;">
 	@foreach($courses as $course)
-  @if($course->isFull()) @continue @endif
+  <!-- @if($course->isFull()) @continue @endif -->
     <li>
       @include('partials._course')
     </li>
